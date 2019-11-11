6a5d6bbe9c0dcfdd2fd437d213c8de65d6db771b
DouglasDans <douglas.dans@gmail.com>
2019-11-10 22:30:58 -0300

    gr

---
+++ js/carrinho.js
@@ -0,0 +1,9 @@
+function Add(){
+    alert("Você tem certeza que deseja adicionar o produto?")
+    document.querySelector('.wrapper');
+    document.getElementById("txtn1").value;
+    document.createElement("tr")
+    document.createElement("th")
+    document.createTextNode('24,99');
+    document.createElement("td");
+}
